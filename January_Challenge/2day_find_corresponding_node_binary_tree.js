7f8f761fbed8647f4b7009e62382645b78f71f6f
dornkholland <dornkholland@gmail.com>
2021-01-02 13:13:11 -0700

    started january day 2 challenge

---
+++ January_Challenge/2day_find_corresponding_node_binary_tree.js
@@ -0,0 +1,10 @@
+// Let's plan out exactly what we are going to do here
+// Psueduocode
+
+/*
+start with root node of clone in dfs stack
+pop it off, check if it is equal to target node
+
+if it is, 
+*/
+
