52f66c0da0107a170078e07bfbd80a3520760bb4
dornkholland <dornkholland@gmail.com>
2021-01-02 13:46:07 -0700

    completed second day january challenge problem :)

--- January_Challenge/2day_find_corresponding_node_binary_tree.js
+++ January_Challenge/2day_find_corresponding_node_binary_tree.js
@@ -2,9 +2,24 @@
 // Psueduocode
 
 /*
-start with root node of clone in dfs stack
-pop it off, check if it is equal to target node
+start with original root node 
+we are going to use a recursive implementation and recursively call until we find the clone
+base case: 
+if original = null then return null;
+if original = target then return cloned original
+if left is null then we want to recursively call only right
+if right is null we want to recursively call only left
+the side that doesn't contain the node will return null after the recursive call
 
-if it is, 
 */
 
+const getTargetCopy = function(original, cloned, target) {
+if (original === null) return null
+else if (original === target) return cloned;
+const rightCall = getTargetCopy(original.right, cloned.right, target)
+const leftCall = getTargetCopy(original.left, cloned.left, target)
+
+if (rightCall === null) return leftCall 
+else return rightCall;
+
+};
