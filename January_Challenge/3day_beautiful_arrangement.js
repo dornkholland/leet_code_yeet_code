cdc551588fff97f3ce37835fe74d6a14ee368744
dornkholland <dornkholland@gmail.com>
2021-01-03 19:10:16 -0700

    ive spent way to long on this problem

---
+++ January_Challenge/3day_beautiful_arrangement.js
@@ -0,0 +1,44 @@
+
+//Plan
+/*
+
+start with an array from 1 to n
+create n recursive calls on helper function with sliced off array
+check condition for each popped is true and return values are not 'fail' string
+if so, add to counter 
+
+
+for next call, do  the same thing
+*/
+
+const countArrangement = function(num) {
+	if (num === 1) return 1;
+	const numArray = [];
+	for(let i = 1; i <= num; i++) {
+		numArray.push[i];
+	}
+	const helper = function(numArray, index) {
+		const counter = 0
+		const n = numArray[0];
+		if (numArray.length = 1) {
+			if (!n % index &&!index % n) {
+				return 1;
+			} else return "Failed!";
+		} else {
+			const popped = n;
+			const newIndex = index + 1;
+			for (let i = 0; i < numArray.length; i++) {
+				const call = helper(numArray, newIndex)
+				if (!popped % index && !index % popped ){
+					if (call !== "Failed!") {
+						counter++;
+					}
+				} else return "Failed!";
+			}
+			}
+			return counter;
+		}
+
+
+		return helper(num, 0);
+	}
