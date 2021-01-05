556fb0b3c0f2e76265d3de39484bbf18ce58a0df
dornkholland <dornkholland@gmail.com>
2021-01-04 18:17:27 -0700

    added base case returning null and passed babyyy

--- January_Challenge/4day_merge_two_lists.js
+++ January_Challenge/4day_merge_two_lists.js
@@ -7,8 +7,10 @@
 
 const mergeTwoLists = function (l1, l2) {
   // check whether l1 or l2 is smaller and set that as start
-  const start;
-  const otherTree;
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+  let start;
+  let otherTree;
   if (l1.val < l2.val) {
     start = l1;
     otherTree = l2;
