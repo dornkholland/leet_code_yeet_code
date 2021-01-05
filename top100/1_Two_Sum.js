6dfe02061fbc993b5a0284bc3269644cbe9e5a99
dornkholland <dornkholland@gmail.com>
2021-01-05 15:57:04 -0700

    completed twosum

--- top100/1_Two_Sum.js
+++ top100/1_Two_Sum.js
@@ -3,13 +3,17 @@
 const twoSum = function(nums, target) {
     const visited = new Object();
     // we want to iterate through the nums array 
+    let result;
     nums.forEach((num,i) => {
         // check if target - num exists in the set
         const weWant = target - num;
-        if (visited[weWant] !== undefined) return [visited[weWant], i];
+        if (visited[weWant] !== undefined) {
+            result = [visited[weWant], i];
+        }
         else {
             visited[num] = i;
         }
     })
+    return result;
     
 }
