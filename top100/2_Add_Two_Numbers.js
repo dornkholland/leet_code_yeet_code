1757125c77eaffd2d3a0d3a643bba7d68066e049
dornkholland <dornkholland@gmail.com>
2021-01-05 17:54:02 -0700

    first try add two numbers

---
+++ top100/2_Add_Two_Numbers.js
@@ -0,0 +1,31 @@
+// we need one function to translate l1 and l2 into numbers
+// helper function takes in one tree
+// returns val + recursive call on next node
+// base case when it is called on null return 0
+
+// then, we add the two numbers together
+
+// second function
+// we mod 10 the number and get the last digit
+// we make a new node as the last number
+// take number, subtract remainder, divide by 10
+// recursive call
+// base case when arg is 0
+// return null
+
+var addTwoNumbers = function (l1, l2) {
+  const translateToNumber = (tree) => {
+    if (tree === null) return 0;
+    else return tree.val + 10 * translateToNumber(tree.next);
+  };
+  const sum = translateToNumber(l1) + translateToNumber(l2);
+
+  const secondFunction = (num) => {
+    if (num === 0) return null;
+    const remainder = num % 10;
+    const ourNode = new listNode(remainder);
+    nextNumber = (num - remainder) / 10;
+    ourNode.next = secondFunction(nextNumber);
+  };
+  return secondFunction(num);
+};
