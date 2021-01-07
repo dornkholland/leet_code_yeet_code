02d8dc4495fdb948d949e7accfe84761edc67443
dornkholland <dornkholland@gmail.com>
2021-01-06 17:46:49 -0700

    totally different approarch for 5day jan, trying to refactor

--- January_Challenge/5day_kth_missing_positive_number.js
+++ January_Challenge/5day_kth_missing_positive_number.js
@@ -1,21 +1,10 @@
-// start with 1
-// check if it is in array
-// if it is remove it from the array
-// if it isn't increment counter
 
-// if counter becomes k then we want to return that number
-// that sounds like a while looop
+// keep track of counter, importantGuy, index
+// index starts at 0, counter starts at 1
+// while counter !== k
+// when arr[index] === counter increment index
+// when arr[index] !== counter increment counter
 
-var findKthPositive = function(arr, k) {
-    let currentGuy = 1;
-    let importantCounter = 0;
-    let index = 0;
-    while (k !== importantCounter) {
-        if (arr[index] !== currentGuy) {
-            importantCounter++;
-        }
-        currentGuy++;
-        index++;
-    }
-    return arr[index]
-}
+// return counter
+
+
